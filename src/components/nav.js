6ce3721fc9f1df8f8fa2c91bcc68337cac4c0e6d
Arjun Singh <arjunsingh8797@gmail.com>
2022-12-22 16:50:34 -0800

    resume test again mod menu

--- src/components/nav.js
+++ src/components/nav.js
@@ -172,7 +172,7 @@ const Nav = ({ isHome }) => {
   );
 
   const ResumeLink = (
-    <a className="resume-button" href="/resume1.png" target="_blank" rel="noopener noreferrer">
+    <a className="resume-button" href="/rtest.pdf" target="_blank" rel="noopener noreferrer">
       Resume
     </a>
   );
