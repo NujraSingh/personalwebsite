ef557d5f68b05223f8f335949d1562da30903541
Arjun Singh <arjunsingh8797@gmail.com>
2023-02-12 22:42:19 -0800

    80k hero

--- src/components/sections/hero.js
+++ src/components/sections/hero.js
@@ -61,7 +61,7 @@ const Hero = () => {
   const one = <h1>Hi, my name is</h1>;
   const two = <h2 className="big-heading">Arjun Singh.</h2>;
   const three = (
-    <h3 className="big-heading">I make social change look and sound more compelling</h3>
+    <h3 className="big-heading">I make social reform look and sound more compelling</h3>
   );
   const four = (
     <>
